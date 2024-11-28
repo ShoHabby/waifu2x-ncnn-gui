bbc959b55271394c9c6022570c36068e0894dbc8
Christophe Savard <christophe_savard@hotmail.ca>
2024-11-28 14:42:33 -0500

    UI first pass

--- Avalonia/ViewModels/MainWindowViewModel.cs
+++ Avalonia/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,15 @@
 ﻿
+using System.Collections.ObjectModel;
+
 namespace Waifu2x.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    public ReadOnlyCollection<string> ScaleFactors { get; } = new(["1", "2", "4", "8", "16", "32"]);
+
+    public ReadOnlyCollection<string> DenoiseLevels { get; } = new(["0", "1", "2", "3"]);
+
+    public ReadOnlyCollection<string> OutputFormats { get; } = new(["PNG", "JPG", "WEBP"]);
+
+    public ObservableCollection<string> Log { get; } = [];
 }
